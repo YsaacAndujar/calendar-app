c483caafcfad3576e130eadbbc4b5aa7badc1f7a
Ysaac Andujar Binet <ysaac@codika.net>
2023-09-26 15:17:30 -0400

    bg calendar

--- src/components/calendar/CalendarScreen.jsx
+++ src/components/calendar/CalendarScreen.jsx
@@ -1,10 +1,24 @@
 import { Navbar } from 'components/ui/Navbar'
 import React from 'react'
+import {Calendar, momentLocalizer} from 'react-big-calendar'
+import moment from 'moment'
+import 'react-big-calendar/lib/css/react-big-calendar.css'
 
+const localizer = momentLocalizer(moment)
+const events = [{
+  title:"cumple jefe",
+  start: moment().toDate(),
+  end: moment().add(2, 'hours').toDate(),
+  bgColor: '#fafafa'
+}]
 export const CalendarScreen = () => {
   return (
-    <div>
+    <div className='calendar-screen'>
       <Navbar/>
+      <Calendar
+        localizer={localizer}
+        events={events}
+      />
     </div>
   )
 }
